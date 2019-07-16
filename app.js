0a954badd9f5da8d14cf2e16c96e686e36876acd
NicoGrapsas <nico.grapsas@gmail.com>
2019-07-16 19:40:05 -0300

    Example slack app

---
+++ app.js
@@ -0,0 +1,17 @@
+ // Initialize using signing secret from environment variables
+const { createEventAdapter } = require('@slack/events-api');
+const slackEvents = createEventAdapter(process.env.SLACK_SIGNING_SECRET);
+const port = process.env.PORT || 3000;
+
+// Attach listeners to events by Slack Event "type". See: https://api.slack.com/events/message.im
+slackEvents.on('message', (event) => {
+  console.log(`Received a message event: user ${event.user} in channel ${event.channel} says ${event.text}`);
+});
+
+// Handle errors (see `errorCodes` export)
+slackEvents.on('error', console.error);
+
+// Start a basic HTTP server
+slackEvents.start(port).then(() => {
+  console.log(`server listening on port ${port}`);
+});
