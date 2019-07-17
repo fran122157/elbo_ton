72561bf669caa71cdbcf12dcbea06ec49978cc0a
Francisco Villarejo <francisco.villarejo@mercadolibre.com>
2019-07-17 15:10:30 -0300

    ping test

--- app.js
+++ app.js
@@ -20,6 +20,13 @@ app.post('/slack/commands', (req, res) => {
     });
 });
 
+app.post('/slack/commands/ping', (req, res) => {
+  res.status(200).send({
+      response_type: "in_channel",
+      text: "pong :raqueta_y_pelota_de_tenis_de_mesa:"
+  });
+});
+
 // Attach listeners to events by Slack Event "type". See: https://api.slack.com/events/message.im
 slackEvents.on('message', (event)=> {
   console.log(`Received a message event: user ${event.user} in channel ${event.channel} says ${event.text}`);
